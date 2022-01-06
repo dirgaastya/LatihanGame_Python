1b16bb0913a848a6810c403f8674210aff154e2d
dirgaastya <astya444@gmail.com>
2022-01-06 20:03:35 +0700

    gameover , score, sound

--- game.py
+++ game.py
@@ -21,6 +21,10 @@ keys = {
 running = True
 playerPos = [100,100] #Posisi awal pemain
 
+exitcode = 0
+EXIT_CODE_GAME_OVER = 0
+EXIT_CODE_WIN =1
+
 score = 0
 arrows=[]
 
@@ -38,6 +42,22 @@ arrow = pg.image.load("resources/images/bullet.png")
 enemy_img = pg.image.load("resources/images/badguy.png")
 healthBar = pg.image.load("resources/images/healthbar.png")
 health = pg.image.load("resources/images/health.png")
+gameOver = pg.image.load("resources/images/gameover.png")
+youWin = pg.image.load("resources/images/youwin.png")
+
+# Load audio
+pg.mixer.init()
+hit_sound = pg.mixer.Sound("resources/audio/explode.wav")
+enemyHit = pg.mixer.Sound("resources/audio/enemy.wav")
+shoot = pg.mixer.Sound("resources/audio/shoot.wav")
+hit_sound.set_volume(0.05)
+enemyHit.set_volume(0.05)
+shoot.set_volume(0.05)
+# Bgm
+pg.mixer.music.load("resources/audio/moonlight.wav")
+pg.mixer.music.play(-1, 0.0)
+pg.mixer.music.set_volume(0.25)
+
 # Game Loop
 while(running):
     # Clear Screen
@@ -102,6 +122,7 @@ while(running):
         if enemy_rect.left < 64:
             enemies.pop(index)
             health_point -= randint(5,20)
+            hit_sound.play()
             print("Oh tidak, kita diserang!!")
         
         # 6.2.2 Check for collisions between enemies and arrows
@@ -115,6 +136,7 @@ while(running):
                 score += 1
                 enemies.pop(index)
                 arrows.pop(index_arrow)
+                enemyHit.play()
                 print("Boom! mati kau!")
                 print("Score: {}".format(score))
             index_arrow += 1
@@ -150,7 +172,7 @@ while(running):
         # Fire
         if event.type == pg.MOUSEBUTTONDOWN:
             arrows.append([angle, new_playerPos[0]+32, new_playerPos[1]+32])
-
+            shoot.play()
         #check keydowm / key up
         if event.type == pg.KEYDOWN:
             if event.key == K_w:
@@ -180,4 +202,31 @@ while(running):
     elif keys["left"]:
         playerPos[0] -= 5 # -x
     elif keys["right"]:
-        playerPos[0] += 5 # + yw
+        playerPos[0] += 5 # + y
+    
+    time_getTicks = pg.time.get_ticks()
+
+    if  time_getTicks > countdown_timer:
+        running = False
+        exitcode = EXIT_CODE_WIN
+    if health_point <= 0:
+        running = False
+        exitcode = EXIT_CODE_GAME_OVER
+
+if exitcode == EXIT_CODE_GAME_OVER:
+    screen.blit(gameOver, (0,0))
+else:
+    screen.blit(youWin, (0,0))
+
+text = font.render("Score: {}".format(score), True, (255,255,255))
+textRect = text.get_rect()
+textRect.centerx=screen.get_rect().centerx
+textRect.centery=screen.get_rect().centery + 24
+screen.blit(text, textRect)
+
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            exit(0)
+    pg.display.flip()
